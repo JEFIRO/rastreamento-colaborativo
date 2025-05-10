7b3c5466817b22846ae3a9457364404122e27b78
JEFIRO <150871603+JEFIRO@users.noreply.github.com>
2025-05-10 20:32:20 -0300

    initial commit

---
+++ src/main/java/com/jefiro/rastreamento/android/RastreamentoColaborativoApplication.java
@@ -0,0 +1,13 @@
+package com.jefiro.rastreamento.android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RastreamentoColaborativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RastreamentoColaborativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jefiro/rastreamento/android/RastreamentoColaborativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefiro.rastreamento.android;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastreamentoColaborativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
